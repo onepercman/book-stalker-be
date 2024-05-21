8962a01d8922ebe6eb8b5ffe6515578e2530193a
onepercman <onepercman@gmail.com>
2024-05-21 23:15:02 +0700

    🚀 update schedule

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,5 +1,5 @@
 import { UserDocument } from "@/user/user.schema"
-import { BadRequestException, Inject, Injectable } from "@nestjs/common"
+import { Inject, Injectable } from "@nestjs/common"
 import { REQUEST } from "@nestjs/core"
 import { InjectModel } from "@nestjs/mongoose"
 import { Model } from "mongoose"
@@ -17,8 +17,6 @@ export class ScheduleService {
   }
 
   async create({ title, time }: Pick<ScheduleDocument, "time" | "title">) {
-    const shadow = await this.scheduleModel.find({ userId: this.request.user._id, time: { $eq: new Date(time) } })
-    if (shadow) return new BadRequestException("Trùng với lịch đã tồn tại")
     const schedule = new this.scheduleModel({ userId: this.request.user._id, title, time })
     return await schedule.save()
   }
